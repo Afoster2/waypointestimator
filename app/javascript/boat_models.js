02c0eebc071cfe8be9cf9e142f5988c608e96ecc
Afoster2 <[email protected]>
2023-07-31 06:22:48 -0500

    updated standard features

--- app/javascript/boat_models.js
+++ app/javascript/boat_models.js
@@ -1,26 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.addEventListener('addStandardFeature', function(e) {
-    e.preventDefault();
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  addStandardFeature(event) {
+    event.preventDefault();
 
     var standardFeatures = document.getElementById('standard_features');
     var newId = new Date().getTime();
-    var regexp = new RegExp('new_standard_features', 'g');
 
     standardFeatures.insertAdjacentHTML('beforeend', `
-    .grid.grid-cols-6.gap-4
-      .col-span-2
-        %label.block.text-sm.font-medium.text-gray-700{:for => "boat_model_standard_features_attributes_${newId}_name"} Name
-        %input.mt-1.focus:ring-indigo-500.focus:border-indigo-500.block.w-full.shadow-sm.sm:text-sm.border-gray-300.rounded-md{:id => "boat_model_standard_features_attributes_${newId}_name", :name => "boat_model[standard_features_attributes][${newId}][name]", :type => "text"}/
-      .col-span-3
-        %label.block.text-sm.font-medium.text-gray-700{:for => "boat_model_standard_features_attributes_${newId}_description"} Description
-        %input.mt-1.focus:ring-indigo-500.focus:border-indigo-500.block.w-full.shadow-sm.sm:text-sm.border-gray-300.rounded-md{:id => "boat_model_standard_features_attributes_${newId}_description", :name => "boat_model[standard_features_attributes][${newId}][description]"}
-      .col-span-2
-        %label.block.text-sm.font-medium.text-gray-700{:for => "boat_model_standard_features_attributes_${newId}_price"} Price
-        %input.mt-1.focus:ring-indigo-500.focus:border-indigo-500.block.w-full.shadow-sm.sm:text-sm.border-gray-300.rounded-md{:id => "boat_model_standard_features_attributes_${newId}_price", :name => "boat_model[standard_features_attributes][${newId}][price]", :type => "number"}/
-      .col-span-1
-        %input.focus:ring-indigo-500.h-4.w-4.text-indigo-600.border-gray-300.rounded{:id => "boat_model_standard_features_attributes_${newId}__destroy", :name => "boat_model[standard_features_attributes][${newId}][_destroy]", :type => "checkbox"}/
-        %label.ml-2.block.text-sm.text-gray-900{:for => "boat_model_standard_features_attributes_${newId}__destroy"} Remove this feature
-
+      <div class="grid grid-cols-6 gap-4">
+        <div class="col-span-2">
+          <label for="boat_model_standard_features_attributes_${newId}_name">Name</label>
+          <input type="text" name="boat_model[standard_features_attributes][${newId}][name]" id="boat_model_standard_features_attributes_${newId}_name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+        </div>
+        <div class="col-span-2">
+          <label for="boat_model_standard_features_attributes_${newId}_description">Description</label>
+          <textarea name="boat_model[standard_features_attributes][${newId}][description]" id="boat_model_standard_features_attributes_${newId}_description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
+        </div>
+        <div class="col-span-1">
+          <label for="boat_model_standard_features_attributes_${newId}_price">Price</label>
+          <input type="number" name="boat_model[standard_features_attributes][${newId}][price]" id="boat_model_standard_features_attributes_${newId}_price" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+        </div>
+        <div class="col-span-1">
+          <button onclick="this.parentNode.parentNode.remove()" type="button" class="my-7 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Remove</button>
+        </div>
+      </div>
     `);
-  });
-});
+  }
+}
--- app/javascript/controllers/boat_model_controller.js
+++ app/javascript/controllers/boat_model_controller.js
@@ -8,21 +8,19 @@ export default class extends Controller {
     var newId = new Date().getTime();
 
     standardFeatures.insertAdjacentHTML('beforeend', `
-      <div class="field">
-        <label for="boat_model_standard_features_attributes_${newId}_name">Name</label>
-        <input type="text" name="boat_model[standard_features_attributes][${newId}][name]" id="boat_model_standard_features_attributes_${newId}_name">
-      </div>
-      <div class="field">
-        <label for="boat_model_standard_features_attributes_${newId}_description">Description</label>
-        <textarea name="boat_model[standard_features_attributes][${newId}][description]" id="boat_model_standard_features_attributes_${newId}_description"></textarea>
-      </div>
-      <div class="field">
-        <label for="boat_model_standard_features_attributes_${newId}_price">Price</label>
-        <input type="number" name="boat_model[standard_features_attributes][${newId}][price]" id="boat_model_standard_features_attributes_${newId}_price">
-      </div>
-      <div class="field">
-        <input type="checkbox" name="boat_model[standard_features_attributes][${newId}][_destroy]" id="boat_model_standard_features_attributes_${newId}__destroy">
-        <label for="boat_model_standard_features_attributes_${newId}__destroy">Remove this feature</label>
+      <div class="grid grid-cols-6 gap-4 mb-7">
+        <div class="col-span-2">
+          <input type="text" name="boat_model[standard_features_attributes][${newId}][name]" id="boat_model_standard_features_attributes_${newId}_name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+        </div>
+        <div class="col-span-2">
+          <input type="text" name="boat_model[standard_features_attributes][${newId}][description]" id="boat_model_standard_features_attributes_${newId}_description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+        </div>
+        <div class="col-span-1">
+          <input type="number" name="boat_model[standard_features_attributes][${newId}][price]" id="boat_model_standard_features_attributes_${newId}_price" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+        </div>
+        <div class="col-span-1">
+          <button onclick="this.parentNode.parentNode.remove()" type="button" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Remove</button>
+        </div>
       </div>
     `);
   }
