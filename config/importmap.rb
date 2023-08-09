feadde4ad1af20315f79d5544094ec02be01b058
Afoster2 <[email protected]>
2023-08-09 04:30:03 -0500

    basics

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,14 +1,15 @@
 # Pin npm packages by running ./bin/importmap
 
+# Application
 pin "application", preload: true
+
+# Hotwired libraries
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Controllers
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "stimulus", to: "stimulus/lib/index.js", preload: true
-pin_all_from "https://unpkg.com/stimulus@3.2.1/dist/", under: "stimulus"
-pin "stimulus", to: "https://unpkg.com/stimulus@3.2.1/dist/stimulus.js"
+
+# External libraries
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
-
-
-
