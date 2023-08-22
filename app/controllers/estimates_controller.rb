c5245ad2a9872dc7b739d54ac0ac3ee10c1d8a18
Afoster2 <[email protected]>
2023-08-22 05:20:54 -0500

    updated estimate views for edit

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,6 +1,7 @@
 class EstimatesController < ApplicationController
   before_action :set_estimate, only: %i[ show edit update destroy ]
   before_action :set_user, only: [:create, :update]
+  before_action :set_default_boat_model, only: [:new]
 
   # GET /estimates or /estimates.json
   def index
@@ -9,20 +10,22 @@ class EstimatesController < ApplicationController
 
   # GET /estimates/1 or /estimates/1.json
   def show
-    @estimate = Estimate.find(params[:id])
-    puts @estimate.inspect
+    @boat_model = @estimate.boat_model
   end
 
   # GET /estimates/new
   def new
     @estimate = Estimate.new
     @boat_model = BoatModel.first
+    unless @boat_model
+      flash[:alert] = "Please add a boat model first."
+      redirect_to boat_models_path # Assuming you have a boat models controller and path
+    end
   end
 
   # GET /estimates/1/edit
   def edit
-    @estimate = Estimate.find(params[:id])
-    @boat_model = @estimate.boat_model
+    @boat_model = @estimate.boat_model || BoatModel.first
   end
 
   # POST /estimates or /estimates.json
@@ -33,65 +36,18 @@ class EstimatesController < ApplicationController
     respond_to do |format|
       if @estimate.save
 
-        # Save power options
-        params[:estimate][:power_options_ids]&.each do |power_option_id|
-          @estimate.estimate_power_options.create(power_option_id: power_option_id)
-        end
-
-        # Save console options
-        params[:estimate][:console_options_ids]&.each do |console_option_id|
-          @estimate.estimate_console_options.create(console_option_id: console_option_id)
-        end
-
-        # Save factory options
-        params[:estimate][:factory_options_ids]&.each do |factory_option_id|
-          @estimate.estimate_factory_options.create(factory_option_id: factory_option_id)
-        end
-
-        # Save gauge upgrades
-        params[:estimate][:gauge_upgrades_ids]&.each do |gauge_upgrade_id|
-          @estimate.estimate_gauge_upgrades.create(gauge_upgrade_id: gauge_upgrade_id)
-        end
-
-        # Save seating options
-        params[:estimate][:seating_options_ids]&.each do |seating_option_id|
-          @estimate.estimate_seating_options.create(seating_option_id: seating_option_id)
-        end
-
-        # Save aluminum options
-        params[:estimate][:aluminum_options_ids]&.each do |aluminum_option_id|
-          @estimate.estimate_aluminum_options.create(aluminum_option_id: aluminum_option_id)
-        end
-
-        # Save lighting options
-        params[:estimate][:lighting_options_ids]&.each do |lighting_option_id|
-          @estimate.estimate_lighting_options.create(lighting_option_id: lighting_option_id)
-        end
-
-        # Save finishing options
-        params[:estimate][:finishing_options_ids]&.each do |finishing_option_id|
-          @estimate.estimate_finishing_options.create(finishing_option_id: finishing_option_id)
-        end
-
-        # Save cooler options
-        params[:estimate][:cooler_options_ids]&.each do |cooler_option_id|
-          @estimate.estimate_cooler_options.create(cooler_option_id: cooler_option_id)
-        end
-
-        # Save wetsound packages
-        params[:estimate][:wetsound_packages_ids]&.each do |wetsound_package_id|
-          @estimate.estimate_wetsound_packages.create(wetsound_package_id: wetsound_package_id)
-        end
-
-        # Save additional options
-        params[:estimate][:additional_options_ids]&.each do |additional_option_id|
-          @estimate.estimate_additional_options.create(additional_option_id: additional_option_id)
-        end
-
-        # Save trailer upgrades
-        params[:estimate][:trailer_upgrades_ids]&.each do |trailer_upgrade_id|
-          @estimate.estimate_trailer_upgrades.create(trailer_upgrade_id: trailer_upgrade_id)
-        end
+        @estimate.power_option_ids = params[:estimate][:power_options_ids]
+        @estimate.console_option_ids = params[:estimate][:console_options_ids]
+        @estimate.factory_option_ids = params[:estimate][:factory_options_ids]
+        @estimate.gauge_upgrade_ids = params[:estimate][:gauge_upgrades_ids]
+        @estimate.seating_option_ids = params[:estimate][:seating_options_ids]
+        @estimate.aluminum_option_ids = params[:estimate][:aluminum_options_ids]
+        @estimate.lighting_option_ids = params[:estimate][:lighting_options_ids]
+        @estimate.finishing_option_ids = params[:estimate][:finishing_options_ids]
+        @estimate.cooler_option_ids = params[:estimate][:cooler_options_ids]
+        @estimate.wetsound_package_ids = params[:estimate][:wetsound_packages_ids]
+        @estimate.additional_option_ids = params[:estimate][:additional_options_ids]
+        @estimate.trailer_upgrade_ids = params[:estimate][:trailer_upgrades_ids]
 
 
         format.html { redirect_to @estimate, notice: 'Estimate was successfully created.' }
@@ -111,80 +67,22 @@ class EstimatesController < ApplicationController
         params[:estimate][:customer_id] = @estimate.customer_id
       end
 
+      @estimate.boat_model_id = params[:estimate][:boat_model_id]
       if @estimate.update(estimate_params)
 
-        @estimate.estimate_power_options.destroy_all
-        @estimate.estimate_console_options.destroy_all
-        @estimate.estimate_factory_options.destroy_all
-        @estimate.estimate_gauge_upgrades.destroy_all
-        @estimate.estimate_seating_options.destroy_all
-        @estimate.estimate_aluminum_options.destroy_all
-        @estimate.estimate_lighting_options.destroy_all
-        @estimate.estimate_finishing_options.destroy_all
-        @estimate.estimate_cooler_options.destroy_all
-        @estimate.estimate_wetsound_packages.destroy_all
-        @estimate.estimate_additional_options.destroy_all
-        @estimate.estimate_trailer_upgrades.destroy_all
+        @estimate.power_option_ids = params[:estimate][:power_options_ids]
+        @estimate.console_option_ids = params[:estimate][:console_options_ids]
+        @estimate.factory_option_ids = params[:estimate][:factory_options_ids]
+        @estimate.gauge_upgrade_ids = params[:estimate][:gauge_upgrades_ids]
+        @estimate.seating_option_ids = params[:estimate][:seating_options_ids]
+        @estimate.aluminum_option_ids = params[:estimate][:aluminum_options_ids]
+        @estimate.lighting_option_ids = params[:estimate][:lighting_options_ids]
+        @estimate.finishing_option_ids = params[:estimate][:finishing_options_ids]
+        @estimate.cooler_option_ids = params[:estimate][:cooler_options_ids]
+        @estimate.wetsound_package_ids = params[:estimate][:wetsound_packages_ids]
+        @estimate.additional_option_ids = params[:estimate][:additional_options_ids]
+        @estimate.trailer_upgrade_ids = params[:estimate][:trailer_upgrades_ids]
 
-        # Save power options
-        params[:estimate][:power_options_ids]&.each do |power_option_id|
-          @estimate.estimate_power_options.create(power_option_id: power_option_id)
-        end
-
-        # Save console options
-        params[:estimate][:console_options_ids]&.each do |console_option_id|
-          @estimate.estimate_console_options.create(console_option_id: console_option_id)
-        end
-
-        # Save factory options
-        params[:estimate][:factory_options_ids]&.each do |factory_option_id|
-          @estimate.estimate_factory_options.create(factory_option_id: factory_option_id)
-        end
-
-        # Save gauge upgrades
-        params[:estimate][:gauge_upgrades_ids]&.each do |gauge_upgrade_id|
-          @estimate.estimate_gauge_upgrades.create(gauge_upgrade_id: gauge_upgrade_id)
-        end
-
-        # Save seating options
-        params[:estimate][:seating_options_ids]&.each do |seating_option_id|
-          @estimate.estimate_seating_options.create(seating_option_id: seating_option_id)
-        end
-
-        # Save aluminum options
-        params[:estimate][:aluminum_options_ids]&.each do |aluminum_option_id|
-          @estimate.estimate_aluminum_options.create(aluminum_option_id: aluminum_option_id)
-        end
-
-        # Save lighting options
-        params[:estimate][:lighting_options_ids]&.each do |lighting_option_id|
-          @estimate.estimate_lighting_options.create(lighting_option_id: lighting_option_id)
-        end
-
-        # Save finishing options
-        params[:estimate][:finishing_options_ids]&.each do |finishing_option_id|
-          @estimate.estimate_finishing_options.create(finishing_option_id: finishing_option_id)
-        end
-
-        # Save cooler options
-        params[:estimate][:cooler_options_ids]&.each do |cooler_option_id|
-          @estimate.estimate_cooler_options.create(cooler_option_id: cooler_option_id)
-        end
-
-        # Save wetsound packages
-        params[:estimate][:wetsound_packages_ids]&.each do |wetsound_package_id|
-          @estimate.estimate_wetsound_packages.create(wetsound_package_id: wetsound_package_id)
-        end
-
-        # Save additional options
-        params[:estimate][:additional_options_ids]&.each do |additional_option_id|
-          @estimate.estimate_additional_options.create(additional_option_id: additional_option_id)
-        end
-
-        # Save trailer upgrades
-        params[:estimate][:trailer_upgrades_ids]&.each do |trailer_upgrade_id|
-          @estimate.estimate_trailer_upgrades.create(trailer_upgrade_id: trailer_upgrade_id)
-        end
 
         format.html { redirect_to estimate_url(@estimate), notice: "Estimate was successfully updated." }
         format.json { render :show, status: :ok, location: @estimate }
@@ -195,13 +93,13 @@ class EstimatesController < ApplicationController
         # puts "======= DEBUGGING ======="
         # puts estimate_params.inspect
       end
+      puts "Boat Model ID: #{@estimate.boat_model_id}"
     end
   end
 
   # DELETE /estimates/1 or /estimates/1.json
   def destroy
     @estimate.destroy
-
     respond_to do |format|
       format.html { redirect_to estimates_url, notice: "Estimate was successfully destroyed." }
       format.json { head :no_content }
@@ -218,13 +116,18 @@ class EstimatesController < ApplicationController
       @user = current_user
     end
 
-    # Only allow a list of trusted parameters through.
-    def estimate_params
-      params.require(:estimate).permit(:customer_id, :boat_model_id, :subtotal, :tax, :vit, :doc_fee, :registration, :total_price,
-        power_options_ids: [], console_options_ids: [], factory_options_ids: [], gauge_upgrades_ids: [], seating_options_ids: [], 
-        aluminum_options_ids: [], lighting_options_ids: [], finishing_options_ids: [], cooler_options_ids: [], wetsound_packages_ids: [], 
-        additional_option_ids: [], trailer_upgrades_ids: [])
+    def set_default_boat_model
+      @boat_model = BoatModel.first
     end
 
+    # Only allow a list of trusted parameters through.
+    def estimate_params
+      params.require(:estimate).permit(
+        :customer_id, :boat_model_id, :subtotal, :tax, :vit, :doc_fee, :registration, :total_price,
+        power_option_ids: [], console_option_ids: [], factory_option_ids: [], gauge_upgrade_ids: [], seating_option_ids: [], 
+        aluminum_option_ids: [], lighting_option_ids: [], finishing_option_ids: [], cooler_option_ids: [], wetsound_package_ids: [], 
+        additional_option_ids: [], trailer_upgrade_ids: []
+      )
+    end
 
 end
