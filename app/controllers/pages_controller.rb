2df6307fc2143d901402f37d4e735caf72223834
Afoster2 <[email protected]>
2023-11-09 05:49:12 -0600

    updated views for estimate show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       # render the index page as usual
-      @estimates = Estimate.all.order(created_at: :desc).limit(4)
-      @customers = Customer.all.order('first_name ASC').limit(8)
+      @estimates = Estimate.all.order(created_at: :desc).limit(24)
+      @customers = Customer.all.order('first_name ASC').limit(12)
     else
       # Redirect to the login page if no user is signed in
       redirect_to new_user_session_path
